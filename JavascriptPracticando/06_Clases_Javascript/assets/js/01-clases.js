0b771ad7d182b5f03d80185dcf56ce08bd529006
xtalo <117736097+santiagoieshna@users.noreply.github.com>
2023-12-31 21:20:41 +0100

    Vite

--- JavascriptPracticando/06_Clases_Javascript/assets/js/01-clases.js
+++ JavascriptPracticando/06_Clases_Javascript/assets/js/01-clases.js
@@ -10,27 +10,56 @@ let persona2Antigua = new Persona(nombre = 'Ana' ,  edad = 35);
 // #### Forma actual #####
 
 class PersonaClase{
-    nombre = '';
-    codigo = '';
-    frase  = '';
+    static _conteo = 0;
+
+    static incrementarConteo() {
+        PersonaClase._conteo++;
+    }
+
+    static get Conteo(){
+        return this._conteo;
+    }
+
+    nombre         = '';
+    codigo         = '';
+    _frase         = '';
+    comida         = '';
+
     constructor(nombre = 'noName', codigo = 'Sin codigo', frase='Sin frase'){
         this.nombre = nombre;
         this.codigo = codigo;
-        this.frase  = frase;
+        this._frase  = frase;
+        PersonaClase.incrementarConteo();
+    }
+
+    set ComidaFavorita(comida){
+        this.comida=comida.toUpperCase();
+    }
+
+    set frase(frase){
+        this._frase=frase;
+    }
+
+    get frase(){
+        return this._frase;
+    }
+
+    quienSoy(){
+        console.log(`Soy ${this.nombre}, codigo: ${this.codigo}`);
+    }
+
+    miFrase(){
+        this.quienSoy();
+        console.log(`Siempre digo: ${this._frase}`);
     }
 }
 const persona1 = new PersonaClase();
 const persona2 = new PersonaClase('Jaime', 'AB-5', 'Pienso, luego existo')
 
-console.log({persona1});
+// console.log({persona1});
+// console.log({persona2});
+persona2.miFrase();
+persona2.ComidaFavorita = 'Pizza'
 console.log({persona2});
 
-
-// // ******** Sabrina ********
-// palaba = ['p','e','l','i','c','a','n','e'];
-// palabaBraille= []
-
-// for (let i = 0; i < palabra.length; i++) {
-//     palabaBraille[i] = convertirBrailler(palaba[i]);
-
-// }
+console.log(PersonaClase.Conteo);
