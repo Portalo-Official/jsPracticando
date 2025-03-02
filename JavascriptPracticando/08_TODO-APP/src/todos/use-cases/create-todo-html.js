3c80ddaa71b7bf0141e076a108778d6c258cdcb1
Portalo <smiguezc01@iesarroyoharnina.es>
2025-03-03 00:26:19 +0100

    desectruturando Todos en crear-todos

--- JavascriptPracticando/08_TODO-APP/src/todos/use-cases/create-todo-html.js
+++ JavascriptPracticando/08_TODO-APP/src/todos/use-cases/create-todo-html.js
@@ -6,21 +6,22 @@ import { Todo } from "../models/todo.model";
  * @returns {HTMLElement} un elemento HTML.
  */
 export const createTodoHTML = ( todo ) =>{
-
     if(!todo) throw new Error('un TODO es requerido');
-    
+
+    const {done, description, id} = todo;
+
     const html = `
       
             <div class="view">
-                <input class="toggle" type="checkbox" ${ todo.done ? 'checked':''}>
-                <label>${todo.description}</label>
+                <input class="toggle" type="checkbox" ${ done ? 'checked':''}>
+                <label>${description}</label>
                 <button class="destroy"></button>
             </div>
             <input class="edit" value="Create a TodoMVC template">
     `;
     const liElement = document.createElement('li');
     //   <li class="${ todo.done ? 'checked' : ''}" data-id="${todo.id}">
-    liElement.setAttribute('data-id', todo.id);
+    liElement.setAttribute('data-id', id);
     if(todo.done){
         liElement.classList.add('completed')
     }
