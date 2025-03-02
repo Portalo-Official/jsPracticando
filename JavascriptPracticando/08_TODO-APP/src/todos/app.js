6aa49f86b3c1cb41732e95abb221e8f290500219
Portalo <smiguezc01@iesarroyoharnina.es>
2025-03-02 22:30:23 +0100

    feat: creacion use-cases rendering todos

--- JavascriptPracticando/08_TODO-APP/src/todos/app.js
+++ JavascriptPracticando/08_TODO-APP/src/todos/app.js
@@ -1,10 +1,16 @@
+import todoStore from "../store/todo.store";
 import html from "./app.html?raw";
+import { renderTodos } from "./use-cases";
 
 /**
  * 
  * @param {String} elementId - id del tag
  */
 export const App = (elementId) => {
+
+    const displayTodos = () => {
+        const todos = todoStore.getTodos( todoStore.getCurrentFilter() );
+    }
 
     //Cuando la funcion app se llama
     (() => {
---
+++ JavascriptPracticando/08_TODO-APP/src/todos/use-cases/render-todos.js
@@ -0,0 +1,9 @@
+
+/**
+ * 
+ * @param {String} elementId 
+ * @param {Todo} todos 
+ */
+export const renderTodos = ( elementId, todos = [] ) =>{
+    console.log(`renderizo en ${elementId}`);
+}
