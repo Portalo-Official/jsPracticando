e6ab92b10519c36d784fe8e4e7ab1ea4b09f71d4
Portalo <smiguezc01@iesarroyoharnina.es>
2025-03-02 21:38:36 +0100

    todo store implemented

--- JavascriptPracticando/08_TODO-APP/src/store/todo.store.js
+++ JavascriptPracticando/08_TODO-APP/src/store/todo.store.js
@@ -20,6 +20,76 @@ const initStore = ()=>{
     console.log('InitStore 🥑');
 }
 
-export default {
-    initStore,
+const loadStore = () =>{
+    throw new Error('Not implemented');
 }
+
+const getTodo = (filter = Filters.All)=>{
+    
+    switch(filter){
+        case Filters.All:
+            return [...state.todos];
+        case Filters.Completed:
+            return state.todos.filter( todo => todo.done);
+        case Filters.Pendind:
+            // si es false es que no esta acabada
+            return state.todos.filter( todo => !todo.done);
+        default:
+            throw new Error(`Opcion ${filter} no permitida`)
+    }
+}
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = (description) =>{
+    if(!description) throw new Error("Descripcion es requerida");
+    state.todos.push(new Todo(description))
+}
+
+const toggleTodo = (todoId) => {
+     if (!todoId) throw new Error("El id es requerido");
+     state.todos = state.todos.map( todo => {
+        if(todo.id === todoId){
+            todo.done = !todo.done
+        }
+        return todo;
+     });
+}
+
+const deleteTodo = (todoId) => {
+    if (!todoId) throw new Error("El id es requerido");
+
+    state.todos = state.todos.filter( todo => todo.id !== todoId);
+}
+
+const deleteCompleted = () =>{
+    state.todos = state.todos.filter( todo => todo.done);
+}
+/**
+ * 
+ * @param {Filters} newFilter 
+ */
+const setFilter = (newFilter = Filters.All) => {
+    if(Object.keys(Filters).includes(newFilter)){
+        throw new Error(`El filtro ${newFilter} no existe`);
+    }
+    state.filter = newFilter;
+}
+
+const getCurrentFilter = ()=>{
+    return state.filter;
+}
+
+export default {
+  addTodo,
+  deleteCompleted,
+  deleteTodo,
+  getCurrentFilter,
+  getTodo,
+  initStore,
+  loadStore,
+  setFilter,
+  toggleTodo,
+};
