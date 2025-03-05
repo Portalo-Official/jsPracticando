2222b821ebf198737dfe1b3146148db7c84bfbde
Portalo <smiguezc01@iesarroyoharnina.es>
2025-03-05 01:05:59 +0100

    feat: promise rejected

--- JavascriptPracticando/09_Advance/src/concepts/03-promise.js
+++ JavascriptPracticando/09_Advance/src/concepts/03-promise.js
@@ -9,13 +9,22 @@ export const promiseComponent = ( element ) =>{
     const renderHero = ( hero ) =>{
         element.innerHTML = hero.name;
     }
-
+    
+    const renderError = ( error ) =>{
+        element.innerHTML = `
+            <h3>${ error }</h3>
+        `;
+    }
+ 
     const id_1 = '5d86371f2343e37870b91ef1';
     const id_2 = "5d86371f25a058e5b1c8a65e";
     // Reducida, al entrar los mismos elementos en la funcion
-    // findHero( id_1 ).then( renderHero );
+    // findHero( id_1 ).then( renderHero )
+    //  .catch( renderError );
 
-    findHero( id_1 ).then( (hero) => renderHero(hero) );
+    findHero( id_1 )
+        .then( (hero) => renderHero(hero) )
+        .catch( error => renderError( error ));
 }
 
 /**
