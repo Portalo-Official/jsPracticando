0e8878df5d8b3d06c950e680f1a803a56d097642
Portalo <smiguezc01@iesarroyoharnina.es>
2025-03-06 23:36:48 +0100

    feat: init project http-app

---
+++ JavascriptPracticando/10-http-app/src/breakingbad/breakingbad-app.js
@@ -0,0 +1,24 @@
+
+const urlQuote = 'https://api.breakingbadquotes.xyz/v1/quotes';
+
+/**
+ * @returns {Object} quote information
+ */
+const fetchQuote = () =>{
+
+    // fetch()
+
+}
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const BreakingbadApp = ( element ) =>{
+
+    document.querySelector('#app-title').innerText = 'Breakingbad App';
+
+    element.innerText = 'Loading...'
+    console.log("BreakingbadApp");
+
+}
