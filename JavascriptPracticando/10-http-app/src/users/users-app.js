c17e76bf3995d2be4a4fae5c2bbf84bf87d1b3d8
Portalo <smiguezc01@iesarroyoharnina.es>
2025-03-09 00:48:19 +0100

    feat: add-button created

--- JavascriptPracticando/10-http-app/src/users/users-app.js
+++ JavascriptPracticando/10-http-app/src/users/users-app.js
@@ -1,3 +1,4 @@
+import { renderAddButton } from './presentation/render-add-button/render-add-button';
 import { renderButtons } from './presentation/render-buttons/render-buttons';
 import { RenderTable } from './presentation/render-table/render-table';
 import userStore from './store/users.store'
@@ -12,4 +13,5 @@ export const UsersApp = async( element ) => {
     
     RenderTable( element );
     renderButtons(element);
+    renderAddButton( element );
 }
