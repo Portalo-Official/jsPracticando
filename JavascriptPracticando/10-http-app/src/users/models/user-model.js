21e98cf2748c94b21ce821186dfcfe5b2a543daf
Portalo <smiguezc01@iesarroyoharnina.es>
2025-03-08 18:47:05 +0100

    feat: create user-model

---
+++ JavascriptPracticando/10-http-app/src/users/models/user-model.js
@@ -0,0 +1,29 @@
+
+
+export class User {
+  constructor(id, isActive=false, balance, avatar,
+             firstName, lastName, gender) {
+              
+    this.id = id;
+    this.isActive=isActive;
+    this.balance=balance;
+    this.avatar= avatar;
+    this.firstName=firstName;
+    this.lastName=lastName;
+    this.gender=gender;
+  }
+  /**
+   * 
+   * @param {Like<User>} userDataLike 
+   */
+  constructor({id, isActive=false, balance, avatar, firstName, lastName, gender}) {
+
+    this.id        = id;
+    this.isActive  = isActive;
+    this.balance   = balance;
+    this.avatar    = avatar;
+    this.firstName = firstName;
+    this.lastName  = lastName;
+    this.gender    = gender;
+  }
+}
